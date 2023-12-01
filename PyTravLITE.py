9b4e6034d73779126177c8df4f2d5f58f9b87de5
Shawn Driscoll <shawndriscoll@hotmail.com>
2023-11-30 22:14:25 -0800

    update

--- PyTravLITE.py
+++ PyTravLITE.py
@@ -11,7 +11,7 @@
 #
 #
 # The Traveller game in all forms is owned by Far Future Enterprises.
-# Copyright 1977 - 2022 Far Future Enterprises.
+# Copyright 1977 - 2024 Far Future Enterprises.
 # Traveller is a registered trademark of Far Future Enterprises.
 #
 
